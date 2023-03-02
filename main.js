7b57dbdefa61c84e2a86bcf9985663a6afdbf75f
Nino Kitoshvili <nino.kitoshvili.3@iliauni.edu.ge>
2023-03-02 15:27:40 +0400

    old code

---
+++ main.js
@@ -0,0 +1,10 @@
+ let dropNav = document.getElementById('drop__nav')
+
+function showNav() {
+    if ( dropNav.style.display=="flex"){
+        dropNav.style.display="none"         
+          }else if(dropNav.style.display="none") {
+             dropNav.style.display="flex"
+          }
+  
+}
